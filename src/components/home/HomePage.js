743ea3e9abd20ab29d2ed631ef854122c3f5a607
Anthony Ngene <anthony.ngene@andela.com>
2017-05-04 17:02:46 +0100

    only react here

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Admin</h1>
+        <p>React, Redux and ...</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learm more</Link>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
---
+++ src/test/sample.spec.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Sample test', () => {
+  it('should pass :)', () => {
+    expect(2 + 2).toEqual(4);
+  })
+});
