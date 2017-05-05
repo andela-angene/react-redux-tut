74337db2e1becb0caa0915954ee7663ce79f27cf
Anthony Ngene <anthony.ngene@andela.com>
2017-05-05 14:35:21 +0100

    update storeconfig

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk, reduxImmutableStateInvariant));
 }
